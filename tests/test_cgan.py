118fa2260e2a91fdeab6fd2806635c5b4830c2f7
ISHIKAWA Atsushi <ishikawa.a.ai@m.titech.ac.jp>
2025-02-04 18:58:39 +0900

    update

--- tests/test_cgan.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append("../")
-
-from conditional_gan.cgan import cgan
-
-target_number = 3
-# Perform CGAN to get MNIST-line figure of target number
-cgan(target_number=target_number)
